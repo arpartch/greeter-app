72f5164d3f9342d02b6c7510d44bc312ad748af0
Athena <athena.partch@gmail.com>
2021-04-01 17:31:23 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import Greeter from './components/Greeter';
+
+const nameArray = [
+  "Melanie",
+  "Emily",
+  "Tom",
+  "Eddie",
+  "Amy",
+  "Adam"
+]
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 className="greeting">Hello I'm Athena!</h1>
+        {
+          nameArray.map(n => <Greeter key={n} name={n}/>)
+        }
     </div>
   );
 }
